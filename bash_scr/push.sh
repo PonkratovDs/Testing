257323a2f65967c26f75e10e0f17e2f725104277
Danil Ponkratov <ponkratov.ds17@physics.msu.ru>
2020-02-27 23:59:51 +0300

    "script update"

--- bash_scr/push.sh
+++ bash_scr/push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -t 100 -p "Enter comment, please: " comment
 
-if ! [ -z "$comment" ]; then
+if [ -n "$comment" ]; then
     git add -A && git commit -m "$comment" && git push
 else    
     echo
